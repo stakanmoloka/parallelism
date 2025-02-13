a7e70f1fa9eca52af9d59dfc1db2d1f4536a92db
Nurshat <n.abdyldaev@g.nsu.ru>
2025-02-13 16:28:21 +0000

    Initial commit

---
+++ with_cmake/build/CMakeFiles/sin_sum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sin_sum.dir/laba1.o"
+  "CMakeFiles/sin_sum.dir/laba1.o.d"
+  "sin_sum"
+  "sin_sum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sin_sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
